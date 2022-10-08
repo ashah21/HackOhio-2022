47bef204c6a0eb516d6518df40d8dfe2e26fbe1d
Bouie782 <bouie.6@osu.edu>
2022-10-08 15:44:47 -0400

    Changing Projects

--- amazon-kinesis-agent-master/tst/com/amazon/kinesis/streaming/agent/tailing/testing/package-info.java
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2014-2017 Amazon.com, Inc. All Rights Reserved.
- */
-/**
- * Test support for the package {@code com.amazon.kinesis.streaming.agent.tailing}.
- */
-package com.amazon.kinesis.streaming.agent.tailing.testing;
